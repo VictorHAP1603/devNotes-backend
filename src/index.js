19e0cee349cac2fb007572ffb22362ecda414351
VictorHugo1603 <vitor.hugo@newpost.com>
2021-05-08 21:54:06 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const routes = require("./routes/routes");
+const cors = require("cors");
 
 require("dotenv").config({ path: "./variables.env" });
 
 const app = express();
 require("./config/db_config");
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
